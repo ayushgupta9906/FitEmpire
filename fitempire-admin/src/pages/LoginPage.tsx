487cb3ff96361b26aba511e2e3fca408bef96a42
Ayush Gupta <ayush-g@newgensoft.com>
2026-08-08 16:57:08 +0530

    feat: apply mirrored dumbbell icon badge to superadmin console

--- fitempire-admin/src/pages/LoginPage.tsx
+++ fitempire-admin/src/pages/LoginPage.tsx
@@ -24,20 +24,18 @@ const DumbbellIcon = ({ size = 24, color = 'currentColor', style = {} }: any) =>
     viewBox="0 0 24 24"
     fill="none"
     stroke={color}
-    strokeWidth="2.5"
+    strokeWidth="2.2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    style={style}
+    style={{ transform: 'scaleX(-1)', ...style }}
   >
-    <path d="m6.5 6.5 11 11"/>
-    <path d="m21 21-1-1"/>
-    <path d="m3 3 1 1"/>
-    <path d="m18 22 4-4"/>
-    <path d="m2 6 4-4"/>
-    <path d="m3 10 7-7"/>
-    <path d="m14 21 7-7"/>
-    <path d="M6.5 12.5 12.5 6.5"/>
-    <path d="m11.5 17.5 6-6"/>
+    <path d="M14.4 14.4 9.6 9.6"/>
+    <path d="M18.657 21.485a2 2 0 1 1-2.829-2.828l.707-.707a2 2 0 1 1 2.829 2.828z"/>
+    <path d="m21.5 21.5-1.4-1.4"/>
+    <path d="M3.929 6.757a2 2 0 1 1 2.828-2.828l.707.707a2 2 0 1 1-2.828 2.828z"/>
+    <path d="m2.5 2.5 1.4 1.4"/>
+    <path d="m8.5 11.5-2.1-2.1a2 2 0 1 1 2.8-2.8l2.1 2.1"/>
+    <path d="m15.5 18.5-2.1-2.1a2 2 0 1 1 2.8-2.8l2.1 2.1"/>
   </svg>
 );
 

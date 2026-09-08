873ddb3370e135c16fd00162d64aad501f845d3b
Ayush Gupta <92868752+ayushgupta9906@users.noreply.github.com>
2026-09-08 17:31:18 +0530

    feat(gyms): add AmenityTagEnum and structured facility tags (#213)

---
+++ fitempire-backend/src/main/java/com/fitempire/modules/gyms/entity/AmenityTag.java
@@ -0,0 +1,16 @@
+package com.fitempire.modules.gyms.entity;
+
+public enum AmenityTag {
+    SWIMMING_POOL,
+    STEAM_SAUNA,
+    MMA_OCTAGON,
+    CARDIO_ZONE,
+    CROSSFIT_RIG,
+    FREE_WEIGHTS,
+    LOCKER_ROOMS,
+    SHOWERS,
+    CAFE_SMOOTHIE_BAR,
+    VALET_PARKING,
+    WIFI_ZONE,
+    PERSONAL_TRAINING
+}
